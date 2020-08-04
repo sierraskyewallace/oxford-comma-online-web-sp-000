aaf43ed2df1b27813084f5fff46307c8545c33ac
Sierra Wallace <sskwallace@gmail.com>
2020-08-04 22:33:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size < 2 
     array.join
     elsif array.size == 2 
-    array.insert (1, "and").join(" ")
+    array.insert(1, "and").join(" ")
   else
     array.pop
   end
