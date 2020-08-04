b4581d148364858a3a7a963c6aa99c664cfaa60b
Sierra Wallace <sskwallace@gmail.com>
2020-08-04 22:47:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+  if array.size < 2 
+    array.join
+    elsif array.size == 2 
+    array.insert(1, "and").join(" ")
+   else array.size > 2 
+     array[-1].insert(0, "and ")
+     array.join(", ")
+   end
+ end
+ 
